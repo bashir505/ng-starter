8678456b78ef43beba748317d7607096db0d042a
bashir505 <bashir505@gmail.com>
2017-06-25 07:50:43 +0300

    added ng2-select multiple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
 
 //High Charts
 import { ChartModule } from 'angular2-highcharts';
@@ -10,10 +11,15 @@ import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 //ag-grid
 import { AgGridModule } from "ag-grid-angular/main";
 
+//ng-select
+import { SelectModule } from 'ng2-select';
+import { TabsModule, ButtonsModule } from 'ng2-bootstrap';
+
 //Components
 import { AppComponent } from './app.component';
 import { HighChartsComponent } from './high-charts/high-charts.component';
 import { AgGridComponent } from './ag-grid/ag-grid.component';
+import { NgTestComponent } from './ng-test/ng-test.component';
 
 export function highchartsFactory() {
   const hc = require('highcharts/highstock');
@@ -30,12 +36,17 @@ export function highchartsFactory() {
     AppComponent,
     HighChartsComponent,
     AgGridComponent,
+    NgTestComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     ChartModule,
+    SelectModule,
+    CommonModule,
+    TabsModule.forRoot(),
+    ButtonsModule.forRoot(),
     AgGridModule
       .withComponents(
       [AgGridComponent,
